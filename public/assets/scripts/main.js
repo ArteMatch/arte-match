46ae48122903529bd768db6790685e30f95d42ae
Johan Contreras <johancontreras.alxlabs@gmail.com>
2025-11-16 22:13:44 -0500

    Ayuda agregada con modal

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -13,3 +13,28 @@ document.getElementById("btnSoyArtista").addEventListener("click", function () {
 document.getElementById("btnBuscoArtista").addEventListener("click", function () {
     window.location.href = "";
 });
+
+const form = document.getElementById('contactoForm');
+const modal = document.getElementById('modal');
+const cerrarModal = document.getElementById('cerrarModal');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    // Mostrar modal
+    modal.style.display = "flex";
+
+    // Limpiar formulario
+    form.reset();
+});
+
+cerrarModal.addEventListener('click', () => {
+    modal.style.display = "none";
+});
+
+// Cerrar si hace click fuera de la ventana
+window.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.style.display = "none";
+    }
+});
